a96cd9dce51110a32901e3425a047b69b7b00cee
Marc Plouhinec <marc.plouhinec@alibaba-inc.com>
2018-12-24 17:29:08 +0800

    Create an empty Spring Boot + React project.

---
+++ src/main/js/app.js
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+function App() {
+    return <div id="app">Hello</div>;
+}
+
+ReactDOM.render(<App/>, document.getElementById('react'));
