637f79b1c72a6674bae39c8dfda194d7975d86d9
Marc Plouhinec <marc.plouhinec@alibaba-inc.com>
2019-01-18 14:46:53 +0800

    Fix certbot installation.

--- infrastructure/25_avl_certman/resources/install_certman.sh
+++ infrastructure/25_avl_certman/resources/install_certman.sh
@@ -44,7 +44,7 @@ echo "Install Certbot"
 apt-get -y install software-properties-common
 add-apt-repository -y ppa:certbot/certbot
 apt-get -y update
-apt-get -y install python-certbot certbot
+apt-get -y install certbot
 
 # Install the Python SDK
 echo "Install Python SDK"
